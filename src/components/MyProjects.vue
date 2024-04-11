dfa66001bc1028351c35328bad555fd0526bb7f2
Matheus Penna <matheus.penna@blitzpay.com.br>
2024-04-11 15:52:10 -0300

    add skeleton

--- src/components/MyProjects.vue
+++ src/components/MyProjects.vue
@@ -4,7 +4,19 @@
     <p class="text-md text-center mb-16">
       Confira abaixo alguns dos projetos feitos ao longo do tempo, bem como projetos que contribuí.
     </p>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8">
+    <div v-if="loading" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8">
+      <skeleton-project />
+      <skeleton-project />
+      <skeleton-project />
+      <skeleton-project />
+      <skeleton-project />
+      <skeleton-project />
+      <skeleton-project />
+      <skeleton-project />
+      <skeleton-project />
+      <skeleton-project />
+    </div>
+    <div v-else class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8">
       <ProjectItem
         v-for="(item, i) in projects"
         :key="`project-${i}`"
@@ -15,13 +27,18 @@
   </section>
 </template>
 <script setup>
+  import SkeletonProject from "@/components/SkeletonProject.vue";
   import ProjectItem from "@/components/ProjectItem.vue";
   import { useProjectsApi } from "@/composable/api/projects"
   const { getProjects } = useProjectsApi();
   const projects = reactive([]);
+  const loading = ref(false);
   onMounted(() => {
+    loading.value = true;
     getProjects().then(data => {
       projects.push(...data);
+    }).finally(() => {
+      loading.value = false;
     })
   })
 </script>
